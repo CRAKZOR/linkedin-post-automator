e90aa1e50d1629a643305ea86d804c289ce7c1fd
henriquelobato <iklobato1@gmail.com>
2023-07-27 19:25:40 -0300

    fix: general

--- main.py
+++ main.py
@@ -1,89 +1,105 @@
+import configparser
 from datetime import timedelta, datetime
 import json
 import random
 import requests
 import schedule
 import openai
-from re import sub
 from time import sleep
+import logging
 
 from scraper import Scraper
 
-
-def custom_print(message):
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print(f"[{current_time}] {message}")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s %(message)s',
+    handlers=[
+        logging.FileHandler("in.log"),
+        logging.StreamHandler()
+    ]
+)
 
 
 def ask_chatgpt(messages, token_limit=150, model="gpt-3.5-turbo"):
-    """
-        see https://platform.openai.com/docs/models/gpt-4 for available engines
-        :param messages:
-        :param token_limit: limit the response length
-        :param model:       gpt model/engine
-        :return:
 
-        messages must be in format ex: [ {"role": "user", "content": "I am Peter"} ... ]
-    """
-
+    request_wait_time_seconds = 1
+    response_text = ''
+    # reason_mapping = {
+    #     "stop": "Reached max_tokens",
+    #     "length": "Response length exceeded max_tokens",
+    #     "function_call": "Function call encountered",
+    #     "content_filter": "Content filter triggered",
+    #     "max_tokens": "Max tokens exceeded",
+    # }
     while True:
         try:
             response = openai.ChatCompletion.create(
-                model       = model,
-                messages    = messages,
-                max_tokens=token_limit
-                # temperature = 0,
-
+                model=model,
+                messages=messages,
+                max_tokens=token_limit,
+                stop=["xx"],
             )
-            custom_print(response["usage"])
 
-            if response.choices[0].finish_reason != "stop":
-                # message was cut off
-                sleep(5)
-                continue
+            logging.info(f'Usage: {response["usage"]}')
 
-            return response.choices[0].message.content.strip()
+            # if response.choices[0].finish_reason in reason_mapping.keys():
+            #     reason_name = reason_mapping[response.choices[0].finish_reason]
+            #     raise Exception(f"Reason: {reason_name}")
+
+            response_text = response.choices[0].message['content'].strip()
+            logging.info(f"Text from GPT: {response_text}")
+            return response_text
+
         except openai.error.RateLimitError:
-            custom_print("Rate limit exceeded. Retrying in 60 seconds...")
-            sleep(60)
+            logging.error(f"Rate limit exceeded. Retrying in {request_wait_time_seconds} seconds...")
         except openai.error.ServiceUnavailableError:
-            custom_print("The server is overloaded or not ready yet.  Retrying in 60 seconds...")
-            sleep(60)
+            logging.error("The server is overloaded or not ready yet.  Retrying in 60 seconds...")
+        except Exception as e:
+            logging.error(f"Error: {e}")
+        finally:
+            with open("out.log", "a") as f:
+                f.write(f"{datetime.now()}: {response_text}\n")
+            request_wait_time_seconds **= 2
+            logging.info(f"Request wait time: {request_wait_time_seconds}")
+            sleep(request_wait_time_seconds)
 
 
-def post_linkedin(payload, cookies ):
+def get_session():
+    if "linkedin_session" not in globals():
+        session = requests.Session()
+        globals()["linkedin_session"] = session
+    return globals()["linkedin_session"]
+
+
+def post_linkedin(payload, cookies):
+    session = get_session()
     cookie_value = "li_at=%s; JSESSIONID=\"%s\"" % (cookies["li_at"], cookies["JSESSIONID"])
     headers = {
-        "accept":                   "application/vnd.linkedin.normalized+json+2.1",
-        "accept-language":          "en-US,en;q=0.9",
-        "content-type":             "application/json; charset=UTF-8",
-        "csrf-token":               cookies["JSESSIONID"],
-        "referrer-policy":          "strict-origin-when-cross-origin, strict-origin-when-cross-origin",
-        "origin":                   "https://www.linkedin.com",
-        "Referrer":                 "https://www.linkedin.com/feed/",
-        "Referrer-Policy":          "strict-origin-when-cross-origin, strict-origin-when-cross-origin",
-        "cookie":                   cookie_value
-        # ... other headers ...
+        "accept": "application/vnd.linkedin.normalized+json+2.1",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json; charset=UTF-8",
+        "csrf-token": cookies["JSESSIONID"],
+        "referrer-policy": "strict-origin-when-cross-origin, strict-origin-when-cross-origin",
+        "origin": "https://www.linkedin.com",
+        "Referrer": "https://www.linkedin.com/feed/",
+        "Referrer-Policy": "strict-origin-when-cross-origin, strict-origin-when-cross-origin",
+        "cookie": cookie_value
     }
 
     post_endpoint = "https://www.linkedin.com/voyager/api/contentcreation/normShares"
 
     try:
-        response = requests.post(post_endpoint, headers=headers, data=payload)
+        response = session.post(post_endpoint, headers=headers, data=payload)
         response.raise_for_status()
 
-        # Check response content if needed
         if response.json().get('someKey', None) == 'expectedValue':
-            # Handle response
             pass
 
     except requests.exceptions.RequestException as e:
-        # Handle exception
-        custom_print(f"Error posting to LinkedIn: {e}")
-        custom_print(response.text)
+        logging.error(f"Error posting to LinkedIn: {e}")
 
 
-def get_file_data (fname, protocol="r"):
+def get_file_data(fname, protocol="r"):
     # Load websites from the JSON file
     with open(fname, protocol) as file:
         if "json" in str.lower(fname):
@@ -94,16 +110,23 @@ def get_file_data (fname, protocol="r"):
 
 
 def main():
-    config      = get_file_data("config.json")
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-    cookies     = config["cookies"]
-    api_key     = config["open_ai_api_key"]
-    urls        = config["websites"]
-    preamble    = config["gpt_preamble"]
-    bio         = config["bio"]
+    settings = config['settings']
+    bio = settings['bio']
+    preamble = settings['gpt_preamble']
+    gpt_token_limit = int(settings['gpt_token_limit'])
+    scrape_char_limit = int(settings['scrape_char_limit'])
+    openai.api_key = settings['open_ai_api_key']
 
-    gpt_token_limit     = config["gpt_token_limit"]
-    scrape_char_limit   = config["scrape_char_limit"]
+    cookies_conf = config['cookies']
+    cookies = {
+        'JSESSIONID': cookies_conf['JSESSIONID'],
+        'li_at': cookies_conf['li_at']
+    }
+
+    urls = config['websites']['websites'].split()
 
     content = []
     for url in urls:
@@ -112,19 +135,17 @@ def main():
             content.append(data)
     random.shuffle(content)
 
-    # Combine preamble, bio, and website content into the correctly formatted messages
-    gpt_messages = [
-                       {"role": "system", "content": preamble},
-                       {"role": "system", "content": bio},
-                   ] + [
-                       {"role": "user", "content": item} for item in content
-                   ]
+    system_messages = [
+        {"role": "system", "content": preamble},
+        {"role": "system", "content": bio},
+    ]
 
-    openai.api_key = api_key
+    user_messages = [
+        {"role": "user", "content": item} for item in content
+    ]
 
-    # print(gpt_messages)
+    gpt_messages = system_messages + user_messages
     gpt_res = ask_chatgpt(gpt_messages, gpt_token_limit)
-    custom_print("Post: " + sub( r'\n+', ' ', gpt_res))
 
     payload = {
         "visibleToConnectionsOnly": False,
@@ -139,36 +160,23 @@ def main():
         "media": []
     }
 
-    post_linkedin( json.dumps(payload), cookies )
+    post_linkedin(json.dumps(payload), cookies )
 
 
 def main_task():
     main()
-    # After the main task is done, schedule the next task
     schedule_next_task()
+    logging.info("Task completed")
 
 
 def schedule_next_task():
-    random_hour     = random.randint(11, 16)           # Random hour between 11am and 4pm inclusive
-    random_minute   = random.randint(0, 59)            # Random minute
-
-    # Compute the next date when the task will run
-    next_run_date = datetime.now() + timedelta(days=2)
-    formatted_next_run_date = next_run_date.strftime('%m/%d/%Y')
-
-    custom_print(f"Scheduled to run on {formatted_next_run_date} at {random_hour}:{random_minute}")
-
-    # Clear all jobs before scheduling a new one
-    schedule.clear()
-
-    schedule.every(2).days.at(f"{random_hour:02d}:{random_minute:02d}").do(main_task)
+    minutes_for_next_task = random.randint(1, 60)
+    logging.info(f"Next task scheduled in {minutes_for_next_task} minutes")
+    schedule.every(minutes_for_next_task).minutes.do(main_task)
+    # schedule.every(minutes_for_next_task).seconds.do(main_task)
 
 
 if __name__ == "__main__":
-    # Run main() once initially
-    main()
-
-    # Start the process by scheduling the first task
     schedule_next_task()
 
     while True:
--- scraper.py
+++ scraper.py
@@ -17,22 +17,16 @@ class Scraper:
     def parse(self, content):
         soup = BeautifulSoup(content, 'html.parser')
 
-        # Removing undesired elements like navigation bars and headers
-        # This is a generic example; adjust according to the specific website
-        for element in soup.find_all(["nav", "header"]):  # You can also look for elements by their class or ID
+        for element in soup.find_all(["href"]):
             element.decompose()
 
-        # Removing script and style elements to avoid non-visible text
         for script in soup(["script", "style"]):
             script.extract()
 
-        # Extract text using the get_text() method
         all_text = soup.get_text()
 
-        # Split the lines, remove leading and trailing whitespace
         lines = (line.strip() for line in all_text.splitlines())
 
-        # Remove empty lines, and multi-space and join
-        all_text_clean = re.sub( r'\s+', ' ', ( ' '.join( line for line in lines if line ) ) )
-
-        return all_text_clean[0:self.character_limit]
+        all_text_clean = re.sub(r'\s+', ' ', (' '.join(line for line in lines if line)))
+        all_text_clean = all_text_clean.replace("  ", " ").replace('\\n', '')
+        return all_text_clean
