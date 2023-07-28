be933703061965dc5ac96f94639a35e04b2b0b1b
henriquelobato <iklobato1@gmail.com>
2023-07-28 01:47:33 -0300

    feat(rss): add RssScrap

--- scraper.py
+++ scraper.py
@@ -1,6 +1,10 @@
+import random
+from configparser import ConfigParser
+
 import requests
 from bs4 import BeautifulSoup
 import re
+import logging
 
 
 class Scraper:
@@ -9,6 +13,7 @@ class Scraper:
         self.character_limit = character_limit
 
     def fetch_content(self):
+        logging.info(f"Fetching content from {self.url}")
         response = requests.get(self.url)
         if response.status_code == 200:
             return self.parse(response.text)
@@ -30,3 +35,32 @@ class Scraper:
         all_text_clean = re.sub(r'\s+', ' ', (' '.join(line for line in lines if line)))
         all_text_clean = all_text_clean.replace("  ", " ").replace('\\n', '')
         return all_text_clean
+
+
+class RssScrap(Scraper):
+
+    def parse(self, content):
+        news = []
+        try:
+            soup = BeautifulSoup(content, 'xml')
+            for item in soup.find_all('item'):
+                news.append({
+                    'title': item.find('title').text,
+                    'link': item.find('link').text,
+                    'description': item.find('description').text,
+                    'thumbnail': item.find('media:thumbnail').get('url'),
+                })
+        except Exception as e:
+            logging.error(f"Error: {e}")
+        finally:
+            random.shuffle(news)
+            chosen_rss = news[0]
+            return chosen_rss
+
+
+if __name__ == '__main__':
+    config = ConfigParser()
+    config.read('config.ini')
+    xss_urls = config.get('websites', 'websites').split(',')
+    r = RssScrap('https://www.entrepreneur.com/latest.rss').fetch_content()
+    print(r)
